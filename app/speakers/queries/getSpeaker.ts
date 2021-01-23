63dd8bc48e9f4d99ade81ef523a7e1684325d578
rakutek <rakutek12@gmail.com>
2021-01-24 04:10:20 +0900

    認証外した

--- app/speakers/queries/getSpeaker.ts
+++ app/speakers/queries/getSpeaker.ts
@@ -4,7 +4,7 @@ import db, { Prisma } from "db"
 type GetSpeakerInput = Pick<Prisma.FindFirstSpeakerArgs, "where">
 
 export default async function getSpeaker({ where }: GetSpeakerInput, ctx: Ctx) {
-  ctx.session.authorize()
+  // ctx.session.authorize()
 
   const speaker = await db.speaker.findFirst({ where })
 
