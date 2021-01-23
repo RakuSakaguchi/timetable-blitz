05d97e2cf620b94f6760884095c7ab45234bcb5d
rakutek <rakutek12@gmail.com>
2021-01-24 05:55:23 +0900

    クエリ かけたー！

--- app/speakers/mutations/createSpeaker.ts
+++ app/speakers/mutations/createSpeaker.ts
@@ -9,3 +9,43 @@ export default async function createSpeaker({ data }: CreateSpeakerInput, ctx: C
 
   return speaker
 }
+
+db.event
+  .create({
+    data: {
+      name: "props.name",
+      start: "array[0].start",
+      end: "array[0].end",
+      place: "props.place",
+      userId: 1,
+      speakers: {
+        create: [
+          {
+            name: "Prisma Day 2020",
+            sub: "",
+            bio: "",
+            social: "",
+            order: 0,
+            start: "",
+            end: "",
+            body: "",
+            img: "",
+            userId: 1,
+          },
+          {
+            name: "Prisma Day 2020",
+            sub: "",
+            bio: "",
+            social: "",
+            order: 0,
+            start: "",
+            end: "",
+            body: "",
+            img: "",
+            userId: 1,
+          },
+        ],
+      },
+    },
+  })
+  .then(console.log)
